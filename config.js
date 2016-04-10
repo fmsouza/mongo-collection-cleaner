65ac42ccaaa71f8a13839315a0cb2c65c74d44f4
Fred Souza <fredericoamsouza@gmail.com>
2016-04-09 21:48:50 -0300

    Turning the variables configurable through environment

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
 	databaseConnection: {
 		mongodb: {
-			user: "riobus",
-			pass: "riobus",
-			dbName: "riobus",
-			host: "mongo",
-			port: "27017"
+			user: process.env.RIOBUS_DB_USER || "riobus",
+			pass: process.env.RIOBUS_DB_PASS || "riobus",
+			dbName: process.env.RIOBUS_DB_NAME || "riobus",
+			host: process.env.RIOBUS_DB_HOST || "mongo",
+			port: process.env.RIOBUS_DB_PORT || "27017"
 		}
 	},
 	ageLimit: {
